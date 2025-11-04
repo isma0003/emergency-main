8d74433068c614e348db09c5f01c809dd0085693
Isabel <isma0003@stud.ek.dk>
2025-11-04 08:40:58 +0100

    tekst

--- js/instruction.js
+++ js/instruction.js
@@ -5,11 +5,17 @@ function is1() {
   console.log("is 1");
   document.querySelector("#ishoved").classList.add("my_color");
   document.querySelector("#brunis").style.fill = "green";
+  document.querySelector(".info-text h2").textContent = "Den kidnappende isbil";
+  document.querySelector(".info-text p").textContent =
+    "Hvis du bliver kidnappet bliver du lavet til en is";
 }
 document.querySelector("#s").addEventListener("click", is2);
 function is2() {
   console.log("is 2");
   document.querySelector("#sol").classList.add("my_scale");
+  document.querySelector(".info-text h2").textContent = "Sådan spotter du den";
+  document.querySelector(".info-text p").textContent =
+    "Den har lyserøde detaljer";
 }
 
 document.querySelector("#brun").addEventListener("click", is3);
@@ -17,4 +23,8 @@ function is3() {
   console.log("is 3");
   document.querySelector("#brunis").classList.add("my_scale");
   document.querySelector("#brunis").style.fill = "green";
+  document.querySelector(".info-text h2").textContent =
+    "Hvis du bliver kidnappet";
+  document.querySelector(".info-text p").textContent =
+    "Hvis du bliver kidnappet ska du ringe til befrierne";
 }
